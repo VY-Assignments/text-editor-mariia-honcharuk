84807c6e60ef940ee8d70ee012b0c6fdae847073
mariia <mariikahoncharuk@gmail.com>
2026-05-25 21:01:58 +0200

    first commit

---
+++ text_reditor/text_reditor/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  text_reditor
+//
+//  Created by Maria Goncharuk on 25.05.2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
