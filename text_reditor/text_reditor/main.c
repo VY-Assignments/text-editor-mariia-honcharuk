821af0bb13158be92a102b0b35cd8083529e72ef
mariia <mariikahoncharuk@gmail.com>
2026-05-25 21:48:19 +0200

    adding buffer logic

--- text_reditor/text_reditor/main.c
+++ text_reditor/text_reditor/main.c
@@ -8,6 +8,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+struct TextBuffer{
+    char** lines;
+    int line_count;
+    int capacity;
+};
+
+struct TextBuffer* allocate(){
+    struct TextBuffer* buffer = (struct TextBuffer*)malloc(sizeof(struct TextBuffer));
+    buffer->capacity = 10;
+    buffer->line_count = 0;
+    buffer->lines = (char**)malloc(buffer->capacity*sizeof(char*));
+    return buffer;
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
