4c16de282319e0e605e807fcc5980a5c096468e6
mariia <mariikahoncharuk@gmail.com>
2026-06-16 00:15:14 +0300

    first try commit

---
+++ OOP_Cipher_Project/OOP_Cipher_Project/OOP_Cipher_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP_Cipher_Project
+//
+//  Created by Maria Goncharuk on 16.06.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
