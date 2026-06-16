eb31106f1a8006f823c230381120f433d57ae17e
mariia <mariikahoncharuk@gmail.com>
2026-06-17 01:48:54 +0300

    adding VigenereCipher encrypt and decrypt

--- OOP_Cipher_Project/OOP_Cipher_Project/OOP_Cipher_Project/cipher.cpp
+++ OOP_Cipher_Project/OOP_Cipher_Project/OOP_Cipher_Project/cipher.cpp
@@ -36,3 +36,61 @@ std::string CaesarCipher::decrypt(const std::string& text) {
     CaesarCipher reverseCipher(-this->key);
     return reverseCipher.encrypt(text);
 }
+
+std::string VigenereCipher::encrypt(const std::string& text) {
+    std::string result = "";
+    int keyIndex = 0;
+    for (char c : text) {
+        if (isalpha(c)) {
+            char textBase;
+            char keyBase;
+            char currentKey = key[keyIndex % key.length()];
+            if (isupper(c)) {
+                textBase = 'A';
+            } else {
+                textBase = 'a';
+            }
+            if (isupper(currentKey)){
+                keyBase = 'A';
+            } else {
+                keyBase = 'a';
+            }
+            int shiftAmount = currentKey - keyBase;
+            char encryptedChar = (c - textBase + shiftAmount) % 26 + textBase;
+            result += encryptedChar;
+            keyIndex++;
+        } else {
+            result += c;
+        }
+    }
+    return result;
+}
+
+std::string VigenereCipher::decrypt(const std::string& text) {
+    std::string result = "";
+    int keyIndex = 0;
+    for (char c : text) {
+        if (isalpha(c)) {
+            char textBase;
+            char keyBase;
+            char currentKey = key[keyIndex % key.length()];
+            if (isupper(c)) {
+                textBase = 'A';
+            } else {
+                textBase = 'a';
+            }
+            if (isupper(currentKey)){
+                keyBase = 'A';
+            } else {
+                keyBase = 'a';
+            }
+            int shiftAmount = currentKey - keyBase;
+            char encryptedChar = (c - textBase - shiftAmount +26) % 26 + textBase;
+            result += encryptedChar;
+            keyIndex++;
+        } else {
+            result += c;
+        }
+    }
+    return result;
+}
